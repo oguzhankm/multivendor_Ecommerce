c88dfc51ae0b6629620eb208cc735bf60305cc9e
oguzhankarahan <oguzhankm@gmail.com>
2023-05-25 17:48:29 +0300

    Yönetici profili bölümü, AdminController kısmı oluşturuldu.

--- config/app.php
+++ config/app.php
@@ -159,6 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -183,6 +184,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
     ])->toArray(),
 
 ];
